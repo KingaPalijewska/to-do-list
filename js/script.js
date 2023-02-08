65c273baf19007da000a76cead04d00cfe00dda0
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-02-08 20:58:55 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,22 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li
-               ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-               ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -34,23 +18,54 @@
         render();
 
     };
-const onFormSubmit = (event) => {
-    event.preventDefault();
-
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-    if (newTaskContent === "") {
-        return;
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
     }
 
-    addNewTask(newTaskContent);
-}
-    
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+               ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+               <button class="js-remove">usuń</button>
+               ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove")
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
+
     const init = () => {
         render();
         const form = document.querySelector(".js-form")
 
-        form.addEventListener("submit", onFormSubmit );
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
